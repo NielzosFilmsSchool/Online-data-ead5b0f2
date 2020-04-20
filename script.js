ed808120aa7cbaa167e6a3c83084725a723e0b2a
NielzosFilms <nielzosfilms@gmail.com>
2020-04-20 13:25:18 +0200

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+var person = {
+    naam: "Zara",
+    score: 36,
+    begin_datum: "21 januari 2016",
+    vrienden: ["Mika", "Josef", "Maria", "Sumail", "Lotte"],
+    gekwalificeerd: true
+};
+
+console.log(eval(person));
